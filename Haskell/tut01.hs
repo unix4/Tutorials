b8078e0c564ca6c52f24bd348331b4955bfcf4fd
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-11 18:34:33 +0200

    Create tut01.hs

---
+++ Haskell/tut01.hs
@@ -0,0 +1,24 @@
+-- Comment
+
+{-
+    Multiline Comment
+-}
+
+import Data.List
+import System.IO ()
+
+-- Int -2^63 2^63
+maxInt = maxBound :: Int
+
+-- Double
+bigFloat = 3.9999999999 + 0.00000000005
+
+-- Bool True or False
+-- Char
+-- Tuple 
+
+always5 :: Int
+always5 = 5
+
+isGreat :: Bool
+isGreat = True 
