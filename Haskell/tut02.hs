d4cbb9c82ea674a727cdee41335932f95d801e31
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-11 18:39:59 +0200

    Create tut02.hs

---
+++ Haskell/tut02.hs
@@ -0,0 +1,16 @@
+import Data.List
+import System.IO
+
+
+sumOfNums = sum [1..1000]
+addEx = 5 + 5
+subEx = 5 - 4
+multEx = 5 * 4
+divEx = 5 / 4
+
+modEx = mod 5 4
+modEx2 = 5 `mod` 4
+
+negNumEx = 5 + (-4)
+num9 = 9 :: Int
+sqrtOf9 = sqrt(fromIntegral num9)
