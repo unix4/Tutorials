a68825e8460594ad66f421ad130c7234e97498a8
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-11 14:56:39 +0200

    Create tut11.erl

---
+++ Erlang/tut11.erl
@@ -0,0 +1,14 @@
+-import(string, [len/1, concat/2, chr/2, substr/3, str/2, to_lower/1, to_upper/1]).
+
+-export([main/0]).
+
+%Head Recursion
+main() ->
+  for(3,1).
+
+for(0, _) ->
+  ok;
+
+for(Max, Min) when Max > 0 ->
+  io:fwrite("Num : ~p\n", [Max]),
+  for(Max-1, Min).
