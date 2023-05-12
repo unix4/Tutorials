35b4a19b63c41688b9fbac87af96e04cdff4e033
unix4 <69163299+unix4@users.noreply.github.com>
2023-05-12 13:04:18 +0200

    Create tut6.hs

---
+++ Haskell/tut6.hs
@@ -0,0 +1,20 @@
+import Data.List
+import System.IO
+
+pow3List = [3^n | n <- [1..10]]
+
+multTable = [[x * y | y <- [1..10]] | x <- [1..10]]
+
+randTuple = (1, "Random Tuple")
+
+bobSmith = ("Bob Smith", 52);
+
+bobsName = fst bobSmith
+
+bobsAge = snd bobSmith
+
+names = ["Bob", "Mary", "Tom"]
+
+addresses =  ["123 Main", "234 North", "567 South"]
+
+namesNAddress = zip names addresses
